7dc4701e6b90731af5b79702637f0d99f99950bb
buu342 <buu342@hotmail.com>
2018-02-10 20:04:34 +0000

    Removed extra parenthesis

--- lua/weapons/weapon_buu_base2.lua
+++ lua/weapons/weapon_buu_base2.lua
@@ -7,7 +7,7 @@ Sadly, my old sweps broke as a result, so those need to be fixed
 - If you came here to steal, Please give me credit, no one will judge you for using part of someone's code :(
 - If you came here to borrow code for your weapon of mass destruction, feel free to do so but please leave Portugal intact, I like it here
 - If you came here for any other reason, sorry I ignored you :((
-Have fun! And bring some tissues because this stuff might be ugly)))
+Have fun! And bring some tissues because this stuff might be ugly
 */
 
 if ( CLIENT ) then
