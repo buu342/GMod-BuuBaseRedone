e882e7607229e6098eb48842a50fdcb158cf4fd1
KM_CM <send.nex@gmail.com>
2025-10-08 17:00:55 +0300

    Half-Life 2 weapon remakes
    -for people to have fun with.

---
+++ lua/weapons/weapon_buu_base2_357.lua
@@ -0,0 +1,29 @@
+DEFINE_BASECLASS "weapon_buu_base2"
+
+SWEP.PrintName = "#weapon_357"
+SWEP.Category = "Half-Life 2 ( Buu Base 2 )"
+SWEP.Spawnable = true
+SWEP.Author = "KM_CM"
+SWEP.Contact = ""
+SWEP.Purpose = ""
+SWEP.Instructions = "Primary to fire, hold secondary to aim down sights."
+
+SWEP.ViewModel = "models/weapons/c_357.mdl"
+SWEP.WorldModel = "models/weapons/w_357.mdl"
+
+SWEP.HoldType = "revolver"
+SWEP.CrosshairType = 3
+
+SWEP.Slot = 1
+SWEP.ViewModelFOV = 54
+
+SWEP.Primary.ClipSize = 6
+SWEP.Primary.DefaultClip = 6
+SWEP.Primary.Delay = 0
+SWEP.Primary.Automatic = false
+SWEP.Primary.Cone = 0 // No, I am not kidding. The HL2 357 also has zero inaccuracy
+SWEP.Primary.Recoil = 5
+SWEP.Primary.Sound = "Weapon_357.Single"
+SWEP.Primary.Damage = 40
+SWEP.Primary.Ammo = "357"
+SWEP.IronSightsPos = Vector( -4.62, 0, .67 )
---
+++ lua/weapons/weapon_buu_base2_crossbow.lua
@@ -0,0 +1,34 @@
+DEFINE_BASECLASS "weapon_buu_base2"
+
+SWEP.PrintName = "#weapon_crossbow"
+SWEP.Category = "Half-Life 2 ( Buu Base 2 )"
+SWEP.Spawnable = true
+SWEP.Author = "KM_CM"
+SWEP.Contact = ""
+SWEP.Purpose = ""
+SWEP.Instructions = "FOR TECHNICAL REASONS, THIS WEAPON CAN NOT DEAL DAMAGE! Primary to fire, hold secondary to use the scope."
+
+SWEP.ViewModel = "models/weapons/c_crossbow.mdl"
+SWEP.WorldModel = "models/weapons/w_crossbow.mdl"
+
+SWEP.Sniper = true
+SWEP.HoldType = "crossbow"
+SWEP.CrosshairType = 1
+
+SWEP.Slot = 3
+SWEP.ViewModelFOV = 54
+
+SWEP.Primary.NumShots = 7
+SWEP.Primary.ClipSize = 1
+SWEP.Primary.DefaultClip = 1
+SWEP.Primary.Delay = .33
+SWEP.Primary.Automatic = false
+SWEP.Primary.Cone = 0
+SWEP.Primary.Recoil = 4
+SWEP.Primary.Sound = "Weapon_Crossbow.Single"
+SWEP.Primary.Damage = 8
+SWEP.Primary.Projectile = "crossbow_bolt"
+SWEP.Primary.ProjectileForce = 2500
+SWEP.Primary.Ammo = "XBowBolt"
+SWEP.SniperZoom = 20
+SWEP.RunArmPos = Vector( 1.358, -6.228, -.94 )
